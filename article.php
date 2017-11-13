10ab3ef556a0c74fc41bdb2b791c1cc9f5714122
Dmitriy Shcherbakov <shcherbakov.dmytriy@gmail.com>
2017-11-13 18:08:07 +0200

    remove unnecessary impors

--- article.php
+++ article.php
@@ -4,8 +4,6 @@ namespace app;
 
 require_once("DBUtils.php");
 
-use app\DBUtils;
-
 $post = DBUtils::getPost($_GET['id']);
 DBUtils::increaseCounter($post->getID(), $post->getCounter());
 
--- blog.php
+++ blog.php
@@ -4,8 +4,6 @@ namespace app;
 
 require_once("DBUtils.php");
 
-use app\DBUtils;
-
 $posts = DBUtils::getAllPostsSortedByCounter();
 
 include("blog.html");
