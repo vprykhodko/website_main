48de4b57c6f96d7da1dd7fce3a6a133c1a449e84
Dmitriy Shcherbakov <shcherbakov.dmytriy@gmail.com>
2017-11-22 15:40:49 +0200

    add files

---
+++ admin/newsletter.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Админ панель</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+
+</body>
+</html>
