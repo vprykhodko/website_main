c48007834e2d9b0fe4c133daf5004d52d51cbb13
Dmitriy Shcherbakov <shcherbakov.dmytriy@gmail.com>
2017-12-05 12:59:00 +0200

    fix

--- webinar/contactForm.php
+++ webinar/contactForm.php
@@ -9,7 +9,23 @@ use app\DBUtils;
 $phone = trim(strip_tags($_POST['phone']));
 $email = trim(strip_tags($_POST['email']));
 
-mail('astudio@web-site.kiev.ua', 'Письмо с web-site.kiev.ua/webinar',
-    'Его номер телефона: '.$phone.'<br />Его e-mail: '.$email,"Content-type:text/html;charset=UTF-8");
+if(empty($_POST['msg']))
+{
+    mail('emma0975182407@gmail.com', 'Письмо с web-site.kiev.ua/webinar',
+        'Его номер телефона: ' . $phone .
+        '<br />Его e-mail: ' . $email ,
+        "Content-type:text/html;charset=UTF-8");
 
-DBUtils::addSubscriber('', $email, $phone, '');
+    DBUtils::addSubscriber('', $email, $phone, '');
+}
+else
+{
+    $msg = trim(strip_tags($_POST['msg']));
+    mail('emma0975182407@gmail.com', 'Письмо с web-site.kiev.ua/webinar',
+        'Его номер телефона: ' . $phone .
+        '<br />Его e-mail: ' . $email .
+        '<br />Сообщение - ' . $msg ,
+        "Content-type:text/html;charset=UTF-8");
+
+    DBUtils::addSubscriber('', $email, $phone, $msg);
+}
